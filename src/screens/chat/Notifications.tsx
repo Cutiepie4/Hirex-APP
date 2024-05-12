c2603ed8e49ac9a8e020429c6e419dd82fa312c0
Cutiepie4 <trviet4302@gmail.com>
2024-05-12 22:50:45 +0700

    fix/notification-integrated

--- src/screens/chat/Notifications.tsx
+++ src/screens/chat/Notifications.tsx
@@ -1,35 +1,62 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 import { deepPurple, orange, placeholderFontStyle, placeholderTextColor, purple, regularPadding, titleFontStyle } from '../../styles/styles'
 import GOOGLE from '../../assets/images/google_logo.png'
 import NO_NOTIFICATIONS from '../../assets/images/no_notifications.png'
 import RootNavigation from '../../route/RootNavigation'
+import { BASE_API } from '@/services/BaseApi'
 
-interface Notification {
-    id: any,
-    title?: string,
-    description?: string,
-    time?: string,
-    isRead?: boolean,
-}
+export interface Notification {
+    id: number,
+    notificationTitle: string,
+    notificationContent: string,
+    notificationCompanyDetail: string,
+    notificationJobDetail: string,
+    createdAt?: string,
+    read?: boolean,
+};
 
 const Notifications = () => {
     const width = useWindowDimensions().width;
-    const [noti, setNoti] = useState<Notification[]>([
-        { isRead: true, title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-        { title: 'Application sent', id: 123, description: 'Applications for Google companies have entered for company review', time: '25 minutes ago' },
-    ]);
+    const [noti, setNoti] = useState<Notification[]>([]);
 
-    const renderItem = ({ item, index }) => (
+    const formatTimeDifference = (timestampString) => {
+        if (!timestampString) return '-';
+
+        const timestamp = new Date(timestampString);
+        const currentTimestamp = new Date();
+
+        const timeDifference = currentTimestamp.getTime() - timestamp.getTime();
+        const timeDifferenceInSeconds = Math.floor(timeDifference / 1000);
+
+        if (timeDifferenceInSeconds < 60) {
+            return `${timeDifferenceInSeconds} giây`;
+        } else if (timeDifferenceInSeconds < 3600) {
+            const minutes = Math.floor(timeDifferenceInSeconds / 60);
+            return `${minutes} phút`;
+        } else if (timeDifferenceInSeconds < 86400) {
+            const hours = Math.floor(timeDifferenceInSeconds / 3600);
+            return `${hours} giờ`;
+        } else {
+            const days = Math.floor(timeDifferenceInSeconds / 86400);
+            return `${days} ngày`;
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await BASE_API.get('/notifications');
+            if (response.status == 200) {
+                setNoti(response.data);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const renderItem = ({ item, index }: { item: Notification, index: number }) => (
         <TouchableOpacity
             key={index}
             style={[{
@@ -41,8 +68,8 @@ const Notifications = () => {
                 marginTop: 10,
                 marginHorizontal: regularPadding,
                 width: width - regularPadding * 2,
-            }, item.isRead && styles.isRead]}
-            onPress={() => RootNavigation.navigate('NotificationDetail')}
+            }, item.read && styles.isRead]}
+            onPress={() => RootNavigation.navigate('NotificationDetail', { notification: item })}
         >
             <View style={{
                 width: '20%',
@@ -55,15 +82,15 @@ const Notifications = () => {
             <View style={{
                 flex: 1
             }}>
-                <Text style={[titleFontStyle]}>{item?.title}</Text>
+                <Text style={[titleFontStyle]}>{item?.notificationTitle}</Text>
                 <Text style={{
                     color: '#524B6B',
                     marginTop: 4,
-                }}>{item?.description}</Text>
-                <Text style={[placeholderFontStyle, { marginTop: 10 }]}>{item?.time}</Text>
+                }}>{item?.notificationContent}</Text>
+                <Text style={[placeholderFontStyle, { marginTop: 10 }]}>{formatTimeDifference(item.createdAt)}</Text>
             </View>
         </TouchableOpacity>
-    )
+    );
 
     return (
         <Container>
